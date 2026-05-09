c856a8f26da7085a5cfee42607962a69522b45f2
Richard Moore <me@ricmoo.com>
2026-05-09 12:44:17 +0200

    Update for LED strip IRAM config for ESP-IDF v6.

--- src/pixels.c
+++ src/pixels.c
@@ -72,6 +72,8 @@ typedef struct {
     rmt_symbol_word_t reset_code;
 } rmt_led_strip_encoder_t;
 
+
+RMT_ENCODER_FUNC_ATTR
 static size_t rmt_encode_led_strip(rmt_encoder_t *encoder, rmt_channel_handle_t channel, const void *primary_data, size_t data_size, rmt_encode_state_t *ret_state) {
     rmt_led_strip_encoder_t *led_encoder = __containerof(encoder, rmt_led_strip_encoder_t, base);
 
